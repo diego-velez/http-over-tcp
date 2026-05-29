7bd677f04e5593be2d5e6b15f848f6334a079297
Diego Vélez <diego.velez.dev@gmail.com>
2026-05-29 18:19:16 -0400

    Connect the Parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,65 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/diego-velez/http-from-scratch-course/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close() // nolint: errcheck
-		defer close(out)
-
-		var line bytes.Buffer
-		buf := make([]byte, 8)
-		for {
-			n, err := f.Read(buf)
-
-			if n > 0 {
-				data := buf[:n]
-				if bytes.ContainsRune(data, '\n') {
-					lines := bytes.Split(data, []byte{'\n'})
-
-					for i, l := range lines {
-						_, err := line.Write(l)
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						// The last segment is not a complete line probs
-						if i == len(lines)-1 {
-							break
-						}
-
-						out <- line.String()
-
-						line.Reset()
-					}
-				} else {
-					_, err := line.Write(data)
-					if err != nil {
-						log.Fatal(err)
-					}
-				}
-			}
-
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				log.Fatal(err)
-			}
-		}
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:42069")
@@ -76,9 +23,14 @@ func main() {
 
 		fmt.Println("New connection accepted!")
 
-		lines := getLinesChannel(conn)
-		for line := range lines {
-			fmt.Printf("read: %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }

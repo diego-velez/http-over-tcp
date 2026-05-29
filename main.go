78facbfa588ec2198fe55102c14f09b07f432642
Diego Vélez <diego.velez.dev@gmail.com>
2026-05-29 14:27:47 -0400

    8 bytes

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 8)
+	for {
+		n, err := f.Read(buf)
+
+		if n > 0 {
+			fmt.Printf("read (%d bytes): %s\n", n, string(buf[:n]))
+		}
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			panic(err)
+		}
+	}
 }
